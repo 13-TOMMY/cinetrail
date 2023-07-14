3e3bd7f49baf3b766fe09f330155346a030ccc05
Tomasz Lis <125186340+13-TOMMY@users.noreply.github.com>
2023-07-14 11:59:46 +0200

    week 2, lesson 1 (first half)

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Axios } from 'axios'
+import {MdKeyboardArrowRight, MdKeyboardArrowLeft} from 'react-icons/md';
 import './Slider.css'
 
-function Slider() {
+function Slider({apiKey, baseUrl}) {
+  
+  const [upcomingMovies, setUpcomingMovies] = useState([])
+  const [index, setIndex] = useState(0)
+  const imageBaseUrl = "https://image.tmdb.org/t/p/original"
+
+  const sliderStyle ={
+    backgroundImage:`url("${imageBaseUrl}${upcomingMovies[index]?.backdrop_path}")`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    height: "60vh",
+    position: "relative"
+  }
+
   return (
-    <div>Slider</div>
+    <div style={sliderStyle}>
+      <div className="slider-overlay"></div>
+        <MdKeyboardArrowRight onClick={handleRight} className="right-arrow"/>
+        <MdKeyboardArrowLeft onClick={handleLeft} className="left-arrow"/>
+        <div className="slider-info">
+          <h1>{upcomingMovies[index]?.title}</h1>
+          <p className="slider-description">{upcomingMovies[index]?.overview.slice(0,130)} ...</p>
+          <p>Release Date: {upcomingMovies[index]?.release_date}</p>
+        </div>
+    </div>
   )
 }
 
--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Slider from '../../components/Slider/Slider';
 import './Homepage.css'
 
-function Homepage() {
+
+function Homepage({apiKey, baseUrl}) {
   return (
-    <div>Homepage</div>
+    <div className="home-container">
+      <Slider apiKey={apiKey} baseUrl={baseUrl}/>
+    </div>
   )
 }
 
