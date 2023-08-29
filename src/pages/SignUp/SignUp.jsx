8b08fb2c387dd4f612401563ef46fbef93337753
Tomasz Lis <125186340+13-TOMMY@users.noreply.github.com>
2023-08-29 11:58:50 +0200

    sign up logic change

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,15 +1,21 @@
-import React, { useState, useTransition } from "react";
+import React, { useState, useContext } from "react";
 import axios from "axios";
-
+import { Link, useNavigate } from "react-router-dom";
 import "./SignUp.css";
-import { useNavigate } from "react-router-dom";
+import { UserContext } from '../../contexts/UserContext';
+import { ThemeContext } from '../../contexts/ThemeContext';
+
 export default function Signup() {
+  const { token } = useContext(UserContext);
+  const { darkMode } = useContext(ThemeContext);
+  const navigate = useNavigate();
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [username, setUsername] = useState("");
   const [signupSuccess, setSignupSuccess] = useState(false);
   const [message, setMessage] = useState("");
-  const navigate = useNavigate()
+
   const handleSignup = (e) => {
     e.preventDefault();
     axios
@@ -27,53 +33,59 @@ export default function Signup() {
           setEmail("");
           setUsername("");
           setPassword("");
-          navigate("/");
+          navigate("/"); // Use navigate for navigation
         }
       })
       .catch((err) => console.log(err));
   };
+
   const handleCancel = () => {
     navigate("/");
   };
+
   return (
-    <div className="signup-container">
-      <form className="signup-form" onSubmit={handleSignup}>
-        <div className="title-container">
-          <h1>Sign Up</h1>
-          <p>Please fill in this form to create an account.</p>
-        </div>
-        <div className="input-wrapper">
-          <label htmlFor="">Email</label>
-          <input
-            type="email"
-            placeholder="Enter Email"
-            name="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </div>
-        <div className="input-wrapper">
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            placeholder="Enter Password"
-            name="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <div className="input-wrapper">
-          <label htmlFor="username">Username</label>
-          <input
-            type="text"
-            placeholder="Enter Username"
-            name="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            required
-          />
+    <div className={darkMode ? "signup-container" : "signup-container signup-light"}>
+      {token ? (
+        <p>You are already logged in.</p>
+      ) : (
+        <form className="signup-form" onSubmit={handleSignup}>
+          <div className="title-container">
+            <h1>Sign Up</h1>
+            <p>Please fill in this form to create an account.</p>
+          </div>
+          <div className={darkMode ? "input-wrapper" : "input-wrapper input-wrapper-light"}>
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              placeholder="Enter Email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div className={darkMode ? "input-wrapper" : "input-wrapper input-wrapper-light"}>
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              placeholder="Enter Password"
+              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <div className={darkMode ? "input-wrapper" : "input-wrapper input-wrapper-light"}>
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              placeholder="Enter Username"
+              name="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+          </div>
           <div className="button-container">
             <button type="reset" className="cancelbtn" onClick={handleCancel}>
               Cancel
@@ -83,12 +95,18 @@ export default function Signup() {
             </button>
           </div>
           {signupSuccess ? (
-            <p className="success-message">You signed up successfully!</p>
+            <p className="success-message">
+              Signed up successfully! <Link to="/signin">Sign in</Link>
+            </p>
           ) : message ? (
             <p>{message}</p>
-          ) : null }
-        </div>
-      </form>
+          ) : (
+            <p className="signin-message">
+              Already have an account? <Link to="/signin">Sign in</Link>
+            </p>
+          )}
+        </form>
+      )}
     </div>
   );
 }
